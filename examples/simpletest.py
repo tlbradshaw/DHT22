9f41e504d0cfcb597ca64dd6515012c8e7ee5cc8
tlbradshaw <terrylbradshaw@gmail.com>
2018-02-22 13:40:49 -0600

    First push to github

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -21,28 +21,55 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import Adafruit_DHT
+import time
+import sys
+import datetime
+from Adafruit_IO import *
 
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT22
+aio = Client('4551326023d44215bc73c6367ad1b8f0')
 
 # Example using a Beaglebone Black with DHT sensor
 # connected to pin P8_11.
-pin = 'P8_11'
+# pin = 'P8_11'
 
 # Example using a Raspberry Pi with DHT sensor
 # connected to GPIO23.
-#pin = 23
+pin = 17
+
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
+while True:
+
+     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+     temperature = temperature * 1.8 + 32
+     temperature = round(temperature, 1)
+     humidity = round(humidity, 1)
+
+     # Note that sometimes you won't get a reading and
+     # the results will be null (because Linux can't
+     # guarantee the timing of calls to read the sensor).
+     # If this happens try again!
+     if humidity is not None and temperature is not None:
+	 sttime = datetime.datetime.now().strftime('%x_%H:%M:%S ')
+#	 with open('templog.log','a') as logfile:
+#	      logfile.write(sttime + 'Temp={0:0.1f}*F Humidity={1:0.1f}%'.format(temperature, humidity) + '\n')
+	 try:
+         	aio.send('office-temperature',temperature)
+         	aio.send('office-humidity', humidity)
+	 except:
+#		with open('temperror.log','a') as errorlog:
+#			errorlog.write(sttime + ' Failure to send sensor data to Adafruit' + '\n')
+		#print(sttime, sys.exc_info()[0])
+	 	pass
+        # print(sttime + 'Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature, humidity))
+     #else:
+         # print('Failed to get reading. Try again!')
+
+	
+     time.sleep(300)
+
